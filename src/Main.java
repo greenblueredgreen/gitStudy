fcec509a1440f20c554a034e07263b8fcafaae5a
ahyun <kaeruruk@naver.com>
2025-02-23 00:37:29 +0900

    채리픽 재실패. 충돌 내용 들고와짐. revert 시도

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+<<<<<<< HEAD
         //feat2 브랜치
+=======
+        //feat1브랜치
+        //feat1브랜치 테스트
+        
+        //1. feat브랜치 push 안하고 다른 브랜치로 이동해보기
+
+        //5. feat2브랜치에 올려야했는데 여기다 올려버렸을때
+        //다른 브랜치에 올려야하는데 잘못 올렸을 때 - feat2
+>>>>>>> 83b9de8 (5. feat2브랜치에 올려야했는데 여기다 올려버렸을때)
     }
 }
